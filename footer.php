892bb60003b5c968f845c4f262d7ca2b9c48dfe9
Ramiro Neher <ramironeher@gmail.com>
2023-02-01 13:53:17 -0300

    Principal

---
+++ footer.php
@@ -0,0 +1,20 @@
+
+        <?php tha_footer_before(); ?>
+
+        <footer class="site-footer">
+
+            <?php tha_footer_top(); ?>
+            
+            <small class="text-center">Todos los derechos reservados</small>
+
+            <?php tha_footer_bottom(); ?>
+            
+        </footer>
+
+        <?php tha_footer_after(); ?>
+
+        <?php tha_body_bottom(); ?>
+
+        <?php wp_footer(  ); ?>
+    </body>
+</html>
