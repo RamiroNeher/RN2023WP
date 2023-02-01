908a62081e985739fe811cce7c5d4b9fb6f23b4c
Ramiro Neher <ramironeher@gmail.com>
2023-02-01 14:17:31 -0300

    Update header.php

--- header.php
+++ header.php
@@ -41,11 +41,11 @@ if ( ! defined( 'ABSPATH' ) ) {
                     <div class="collapse navbar-collapse" id="main-menu">
                         <?php
                         wp_nav_menu(array(
-                            'theme_location' => 'main-menu',
+                            'theme_location' => 'bootstrap-navbar-menu',
                             'container' => false,
                             'menu_class' => '',
                             'fallback_cb' => '__return_false',
-                            'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                            'items_wrap' => '<ul id="%1$s" class="navbar-nav ms-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
                             'depth' => 3,
                             'walker' => new bootstrap_5_wp_nav_menu_walker()
                         ));
